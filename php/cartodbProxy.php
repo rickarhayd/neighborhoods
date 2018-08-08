bfb35d7b1e07a6f4ba28679cc5faaf5433b03eff
rickarhayd <42193708+rickarhayd@users.noreply.github.com>
2018-08-09 09:35:13 +1200

    Update cartodbProxy.php

--- php/cartodbProxy.php
+++ php/cartodbProxy.php
@@ -3,9 +3,9 @@ session_cache_limiter('nocache');
 $cache_limiter = session_cache_limiter();
 function goProxy($dataURL) 
 {
-	$baseURL = 'http://CARTODB-USER-NAME.cartodb.com/api/v2/sql?';
+	$baseURL = 'http://rickarhayd.carto.com/api/v2/sql?';
 	//  					^ CHANGE THE 'CARTODB-USER-NAME' to your cartoDB url!
-	$api = '&api_key=';
+	$api = 'de90dafd44186bcce24bf2d3edf4346079ec2f4f';
 	//				 ^ENTER YOUR API KEY HERE!
 	$url = $baseURL.'q='.urlencode($dataURL).$api;
 	$result = file_get_contents ($url);
