d269bfd53fa2bcdd0f98047dcc971888ae1ee585
rickarhayd <42193708+rickarhayd@users.noreply.github.com>
2018-08-09 13:26:16 +1200

    Update heart.php

--- php/heart.php
+++ php/heart.php
@@ -1,5 +1,5 @@
 <?php
-include '../../../../apis/cartodbProxy.php';
+include 'Z:/Neighborhood/php/cartodbProxy.php';
 //      ^CHANGE THIS TO THE PATH TO YOUR cartodbProxy.php
 $q = "update hoods set loved = loved " .$_POST['op'] . " where cartodb_id = " . $_POST['id'];
 $return = goProxy($q);
