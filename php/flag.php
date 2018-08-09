01f72d4a033d14758360e5516d0f4357c43b4288
rickarhayd <42193708+rickarhayd@users.noreply.github.com>
2018-08-09 13:25:50 +1200

    Update flag.php

--- php/flag.php
+++ php/flag.php
@@ -1,5 +1,5 @@
 <?php
-include '../../../../apis/cartodbProxy.php';
+include 'Z:/Neighborhood/php/cartodbProxy.php';
 //      ^CHANGE THIS TO THE PATH TO YOUR cartodbProxy.php
 $q = "update hoods set flag = true where cartodb_id = " . $_POST['id'];
 $return = goProxy($q);
